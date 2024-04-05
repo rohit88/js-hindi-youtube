0b095c828f615baa3fd58d696a98e3b691770b5d
rohit88 <rohit.shukla088@gmail.com>
2024-04-05 12:52:48 +0000

    Summary of DataTypes

---
+++ 01-basics/datatypeSummary.js
@@ -0,0 +1,25 @@
+const num=100;
+const score=100.3;
+const loggedin=false;
+let outtemp=null;
+let uEmail;
+const id= Symbol('123');
+const Nid=Symbol('123');
+
+console.log(id===Nid);
+console.log(id);
+console.log(Nid);
+
+const heros=["rohit","aman","raj"];
+
+let myobj={
+    name:"abc",
+    age:23,
+}
+
+const MyFunction=function(){
+    console.log('hello');
+}
+
+console.log(typeof outtemp);
+MyFunction();
