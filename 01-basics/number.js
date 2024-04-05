0d3bce044372f96453d44599044a42f1e30c47d7
rohit88 <rohit.shukla088@gmail.com>
2024-04-05 11:00:08 +0000

    Number Check

---
+++ 01-basics/number.js
@@ -0,0 +1,19 @@
+let num=33;
+
+console.log(typeof(num));
+console.log(typeof num);
+console.log(num);
+
+let num1="33abs";
+
+let convertednum=Number(num1)
+console.log(typeof(convertednum));
+console.log(typeof convertednum);
+console.log(convertednum);
+
+let num2=null;
+
+let convertednum1=Number(num2)
+console.log(typeof(convertednum));
+console.log(typeof convertednum);
+console.log(convertednum);
