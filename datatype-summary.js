e5e8cd0a13578851b0a18fc99796b06ae3c24cef
rohit88 <rohit.shukla088@gmail.com>
2024-04-05 12:33:45 +0000

    Datatypesummary

---
+++ datatype-summary.js
@@ -0,0 +1,11 @@
+const num=100;
+const score=100.3;
+const loggedin=false;
+let outtemp=null;
+let uEmail;
+const id= Symbol('123');
+const Nid=Symbol('123');
+
+console.log(id===Nid);
+console.log(id);
+console.log(Nid);
