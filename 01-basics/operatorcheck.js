a5241089f9875754945ea059c8794327203694a6
rohit88 <rohit.shukla088@gmail.com>
2024-04-05 12:18:01 +0000

    OPRCheck

---
+++ 01-basics/operatorcheck.js
@@ -0,0 +1,14 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+
+console.log("2"==2);
+console.log(2==2);
+
+console.log("2"===2);
+console.log(2===2);
