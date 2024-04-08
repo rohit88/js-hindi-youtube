0acd0dd77d5f6f2f838a31e22cf9794e83d06ba0
rohit88 <rohit.shukla088@gmail.com>
2024-04-08 14:18:03 +0000

    Commit fUNCTIONS

---
+++ 03-basics/Functions.js
@@ -0,0 +1,22 @@
+function Sum(num1,num2)
+{
+    let num3=num1+num2;
+    return num3;
+}
+
+console.log(Sum(1,3));
+
+function logmsg(username)
+{
+    return`${username} good morning`;
+}
+
+console.log(logmsg("rohit"));
+
+function ConsolidateAllVal(...val1)
+{
+    return val1;
+}
+
+console.log(ConsolidateAllVal(100,200,300,500));
+
