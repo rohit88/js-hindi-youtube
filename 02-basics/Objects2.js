13ea2ac0a3d902033587e9d2fb9b58e6d0b974b8
rohit88 <rohit.shukla088@gmail.com>
2024-04-08 13:12:23 +0000

    Object again

---
+++ 02-basics/Objects2.js
@@ -0,0 +1,60 @@
+//const tinderUser= new Object();
+const tinderUser={};
+tinderUser.id=123;
+tinderUser.name="anc",
+tinderUser.mail="anc@123.com"
+
+const regularUser={
+    Email:"reguser@gmail.com",
+    Fullname:{
+        username:{
+        Fname:"Rohit",
+        Lname:"Shukla",
+        }
+    }
+}
+
+console.log(regularUser.Fullname.username.Fname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+
+const obj3=Object.assign(obj1,obj2);
+
+const obj4= {...obj1,...obj2};
+console.log(obj4);
+
+const faceUser=[
+    {
+        id:1,
+        Name:"Aman",
+    },
+    {
+        id:2,
+        Name:"Rahul",
+    },
+    {
+        id:3,
+        Name:"Raj",
+    },
+    {
+        id:4,
+        Name:"Akshay",
+    },
+    {
+        id:1,
+        Name:"Rajiv",
+    }
+]
+
+console.log(faceUser[1].Name);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("name"));
+
+
+
+
