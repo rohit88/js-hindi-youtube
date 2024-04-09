2d9bcb69cc4bdd8cc44e73897d79900879c68bdf
rohit88 <rohit.shukla088@gmail.com>
2024-04-09 06:52:17 +0000

    Scopefile

---
+++ 03-basics/Scope.js
@@ -0,0 +1,26 @@
+function one()
+{
+    const username="rohit"
+    function two()
+    {
+        const website="youtube"
+        console.log(username);
+    }
+
+    //console.log(website);
+    two();
+}
+one();
+
+
+console.log(addone(10));
+
+function addone(num1)
+{
+    return num1 +2;
+}
+//console.log(addtwo(2));
+const addtwo=function(num2)
+{
+    return num2+3;
+}
